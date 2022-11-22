d808d17241c22c40c4241fd35fb9229a009e9371
UnplugCharger <isaacbyron@gmail.com>
2022-11-22 17:08:26 +0300

    le fin

--- app/static/client.js
+++ app/static/client.js
@@ -23,7 +23,7 @@ function analyze() {
   var loc = window.location;
   xhr.open(
     "POST",
-    `http://0.0.0.0:8000/analyze`,
+    `http://127.0.0.1:8000/analyze`,
     true
   );
   xhr.onerror = function () {
